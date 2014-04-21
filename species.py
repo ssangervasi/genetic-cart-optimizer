03882087013094e6733ab9868fd6e1551944ea4e
ssangervasi <ssangervasi@colgate.edu>
2014-04-21 11:19:07 -0400

    Added python file for species class

---
+++ species.py
@@ -0,0 +1,11 @@
+#www.github.com/ssangervasi/genetic-cart-optimizer
+#(c) 2014 Sebastian Sangervasi
+#
+#
+
+class species:
+
+    def __init__(self, products, sellers, priceMin):
+        return
+        
+        
